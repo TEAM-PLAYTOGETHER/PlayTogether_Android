057d3a712e85a23ead8b5ea562a27ce1ef746ad1
lhb8106 <lhb8106@naver.com>
2022-04-15 02:43:53 +0900

    [CHORE] #6 - delete unuseful code

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -11,7 +11,7 @@ object Versions {
     const val core_ktx_version = "1.3.2"
     const val appcompat_version = "1.2.0"
     const val material_design_version = "1.3.0"
-    const val constraint_layout_version = "2.0.4"
+    const val constraint_layout_version = "2.1.3"
     const val datastore_version = "1.0.0-beta01"
     const val lifecycle_ktx_version = "2.4.0-alpha02"
     const val activity_ktx = "1.2.2"
@@ -22,14 +22,14 @@ object Versions {
     const val kotlinx_serialization_version = "1.0.1"
     const val cardview_version = "1.0.0"
 
-    const val gradle_version = "7.0.2"
+    const val gradle_version = "7.1.2"
     const val ktlint_version = "10.0.0"
 
     const val glide_version = "4.12.0"
     const val glide_compiler_version = "4.11.0"
     const val gson_version = "2.8.6"
     const val retrofit_version = "2.9.0"
-    const val okhttp_version = "4.9.1"
+    const val okhttp_version = "4.11.0"
 
     const val junit_version = "4.13.2"
     const val espresso_version = "3.3.0"
