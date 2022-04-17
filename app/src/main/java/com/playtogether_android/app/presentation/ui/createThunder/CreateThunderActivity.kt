c0097ecc0df3fdb4090693f426f724f83cec2985
권용민 <briandr@naver.com>
2022-04-18 01:19:47 +0900

    [FEAT] #8 \n test

---
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/createThunder/CreateThunderActivity.kt
@@ -0,0 +1,13 @@
+package com.playtogether_android.app.presentation.ui.createThunder
+
+import android.os.Bundle
+import com.playtogether_android.app.databinding.ActivityCreateThunderBinding
+import com.playtogether_android.app.presentation.base.BaseActivity
+import com.playtogether_android.app.R
+
+class CreateThunderActivity : BaseActivity<ActivityCreateThunderBinding>(R.layout.activity_create_thunder) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
