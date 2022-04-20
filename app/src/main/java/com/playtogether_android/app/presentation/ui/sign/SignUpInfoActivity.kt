e9a45b2e6c3da53fc409def77c0c8077f04fa159
lhb8106 <lhb8106@naver.com>
2022-04-20 15:47:59 +0900

    [FEAT] #14 - add string

---
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/sign/SignUpInfoActivity.kt
@@ -0,0 +1,15 @@
+package com.playtogether_android.app.presentation.ui.sign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.playtogether_android.app.R
+import com.playtogether_android.app.databinding.ActivitySignInBinding
+import com.playtogether_android.app.databinding.ActivitySignUpInfoBinding
+import com.playtogether_android.app.presentation.base.BaseActivity
+
+class SignUpInfoActivity : BaseActivity<ActivitySignUpInfoBinding>(R.layout.activity_sign_up_info) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
