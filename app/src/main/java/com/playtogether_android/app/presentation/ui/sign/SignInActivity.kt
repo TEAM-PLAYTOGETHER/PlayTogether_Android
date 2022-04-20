2cb2b16f9fb096f349fb8a74911d2682384cbb27
lhb8106 <lhb8106@naver.com>
2022-04-20 15:06:10 +0900

    [FEAT] #12 - login layout end

--- app/src/main/java/com/playtogether_android/app/presentation/ui/sign/SignInActivity.kt
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/sign/SignInActivity.kt
@@ -3,10 +3,14 @@ package com.playtogether_android.app.presentation.ui.sign
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.playtogether_android.app.R
+import com.playtogether_android.app.databinding.ActivityMainBinding
+import com.playtogether_android.app.databinding.ActivitySignInBinding
+import com.playtogether_android.app.presentation.base.BaseActivity
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : BaseActivity<ActivitySignInBinding>(R.layout.activity_sign_in) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+
     }
+
 }
