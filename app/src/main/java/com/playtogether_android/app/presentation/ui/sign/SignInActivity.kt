da5970e71140726754b46062acf5b81a79da3135
lhb8106 <lhb8106@naver.com>
2022-04-20 13:07:44 +0900

    [FEAT] #12 - add sign string

---
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/sign/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.playtogether_android.app.presentation.ui.sign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.playtogether_android.app.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
