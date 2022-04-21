ead787ee5965c7965bf4aa22b1af09189cfe588b
권용민 <briandr@naver.com>
2022-04-21 21:19:55 +0900

    [FEAT] #16 - 상세보기 디폴트 뷰 레이아웃 완성

---
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/home/ThunderDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.playtogether_android.app.presentation.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.playtogether_android.app.R
+
+class ThunderDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thunder_detail)
+    }
+}
