b2f9d7f484aee3d6dbfeae17e0ca80a9977b4d33
권용민 <briandr@naver.com>
2022-04-22 00:13:10 +0900

    [FEAT] #18 - 커스텀 다이얼로그 완성

---
+++ app/src/main/java/com/playtogether_android/app/util/CustomDialog.kt
@@ -0,0 +1,55 @@
+package com.playtogether_android.app.util
+
+import android.app.Dialog
+import android.content.Context
+import android.view.WindowManager
+import android.widget.TextView
+import androidx.annotation.LayoutRes
+import com.playtogether_android.app.R
+
+class CustomDialog(context:Context, val title:String) {
+    private val dialog = Dialog(context)
+    private lateinit var onClickedListener : ButtonClickListener
+
+    interface ButtonClickListener{
+        fun onClicked(num:Int)
+    }
+    fun setOnClickedListener(listener:ButtonClickListener){
+        onClickedListener=listener
+    }
+
+    fun showChoiceDialog(@LayoutRes layout : Int){
+        dialog.setContentView(layout)
+        dialog.findViewById<TextView>(R.id.tv_dialog_title).text=title
+        dialog.window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window?.setBackgroundDrawableResource(R.drawable.inset_horizontal_58)
+        dialog.show()
+
+        dialog.findViewById<TextView>(R.id.tv_dialog_no).setOnClickListener{
+            dialog.dismiss()
+        }
+        dialog.findViewById<TextView>(R.id.tv_dialog_yes).setOnClickListener{
+            onClickedListener.onClicked(1)
+            dialog.dismiss()
+        }
+    }
+
+    fun showConfirmDialog(@LayoutRes layout : Int){
+        dialog.setContentView(layout)
+        dialog.findViewById<TextView>(R.id.tv_dialog_title).text=title
+        dialog.window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window?.setBackgroundDrawableResource(R.drawable.inset_horizontal_58)
+        dialog.show()
+
+        dialog.findViewById<TextView>(R.id.tv_dialog_yes).setOnClickListener{
+            onClickedListener.onClicked(1)
+            dialog.dismiss()
+        }
+    }
+}
