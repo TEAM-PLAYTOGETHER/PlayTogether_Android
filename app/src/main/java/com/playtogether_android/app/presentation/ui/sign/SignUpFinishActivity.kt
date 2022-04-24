e2fef9f5fbd9f8ec0c92f768b13830d0a2cfe825
lhb8106 <lhb8106@naver.com>
2022-04-24 21:24:25 +0900

    [FEAT] #25 - add string

---
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/sign/SignUpFinishActivity.kt
@@ -0,0 +1,11 @@
+package com.playtogether_android.app.presentation.ui.sign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpFinishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_finish)
+    }
+}
