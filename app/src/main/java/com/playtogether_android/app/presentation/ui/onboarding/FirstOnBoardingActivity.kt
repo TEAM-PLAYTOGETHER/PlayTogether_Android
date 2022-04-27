35a165310cfee8ee78358f322738f4735f75c488
lhb8106 <lhb8106@naver.com>
2022-04-27 21:26:27 +0900

    [FEAT] #28 - add file

---
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/onboarding/FirstOnBoardingActivity.kt
@@ -0,0 +1,12 @@
+package com.playtogether_android.app.presentation.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.playtogether_android.app.R
+
+class FirstOnBoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_on_boarding)
+    }
+}
