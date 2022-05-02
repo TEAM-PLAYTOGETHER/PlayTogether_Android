49c3f1bd978a42fad8bbbca1770b66964f89a1d0
권용민 <briandr@naver.com>
2022-05-02 15:05:28 +0900

    [FEAT] #35- 채팅창 레이아웃 완성

---
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/message/InChattingFragment.kt
@@ -0,0 +1,17 @@
+package com.playtogether_android.app.presentation.ui.message
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.playtogether_android.app.R
+import com.playtogether_android.app.databinding.FragmentInChattingBinding
+import com.playtogether_android.app.presentation.base.BaseFragment
+
+class InChattingFragment : BaseFragment<FragmentInChattingBinding>(R.layout.fragment_in_chatting) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
