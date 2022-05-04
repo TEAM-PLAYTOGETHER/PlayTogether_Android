41761479159bf12e1938d5e5f46d78fc1ccd6f0b
lhb8106 <lhb8106@naver.com>
2022-05-04 14:36:06 +0900

    [FEAT] #47 - logic connection

--- app/src/main/java/com/playtogether_android/app/presentation/ui/onboarding/FirstOnBoardingActivity.kt
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/onboarding/FirstOnBoardingActivity.kt
@@ -3,10 +3,12 @@ package com.playtogether_android.app.presentation.ui.onboarding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.playtogether_android.app.R
+import com.playtogether_android.app.databinding.ActivityFirstOnBoardingBinding
+import com.playtogether_android.app.databinding.ActivitySecondOnboardingBinding
+import com.playtogether_android.app.presentation.base.BaseActivity
 
-class FirstOnBoardingActivity : AppCompatActivity() {
+class FirstOnBoardingActivity : BaseActivity<ActivityFirstOnBoardingBinding>(R.layout.activity_first_on_boarding) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first_on_boarding)
     }
 }
--- app/src/main/java/com/playtogether_android/app/presentation/ui/onboarding/JoinOnBoardingActivity.kt
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/onboarding/JoinOnBoardingActivity.kt
@@ -1,12 +1,74 @@
 package com.playtogether_android.app.presentation.ui.onboarding
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import androidx.core.widget.addTextChangedListener
 import com.playtogether_android.app.R
+import com.playtogether_android.app.databinding.ActivityJoinOnBoardingBinding
+import com.playtogether_android.app.presentation.base.BaseActivity
+import timber.log.Timber
 
-class JoinOnBoardingActivity : AppCompatActivity() {
+class JoinOnBoardingActivity :
+    BaseActivity<ActivityJoinOnBoardingBinding>(R.layout.activity_join_on_boarding) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join_on_boarding)
+
+        initBackBtn()
+        initEditText()
+        editTextWatcher()
     }
+
+    //뒤로가기 버튼 클릭 리스너
+    private fun initBackBtn() {
+        binding.ivJoinOnboardingBack.setOnClickListener {
+            startActivity(Intent(this, SecondOnboardingActivity::class.java))
+            finish()
+        }
+    }
+
+
+    //edittext selector
+    private fun initEditText() {
+        binding.etJoinOnboarding.setOnClickListener {
+            binding.etJoinOnboarding.isSelected = true
+        }
+    }
+
+    //빈칸인지 체크
+    private fun editTextBlankCheck() {
+        if (binding.etJoinOnboarding.text.toString() != "") {
+            binding.etJoinOnboarding.setBackgroundResource(R.drawable.rectangle_border_gray01_radius_10)
+
+            binding.tvJoinOnboardingNext.visibility = View.INVISIBLE
+            binding.tvJoinOnboardingEnter.visibility = View.VISIBLE
+
+            Timber.d("빈칸 아님")
+        } else {
+            binding.tvJoinOnboardingNext.visibility = View.VISIBLE
+            binding.tvJoinOnboardingEnter.visibility = View.INVISIBLE
+            Timber.d("빈칸")
+        }
+    }
+
+    private fun editTextWatcher() = with(binding) {
+        etJoinOnboarding.addTextChangedListener { object:TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                editTextBlankCheck()
+                Timber.d("바뀜")
+            }
+
+        } }
+    }
+
 }
