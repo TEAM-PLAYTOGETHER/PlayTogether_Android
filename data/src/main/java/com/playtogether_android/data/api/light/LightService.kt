bf744212cb5887065b146d55bbb698c0b2ba39f0
username <ieejo716@naver.com>
2022-05-09 18:29:48 +0900

    [CHORE] : 임시 커밋

--- data/src/main/java/com/playtogether_android/data/api/light/LightService.kt
+++ data/src/main/java/com/playtogether_android/data/api/light/LightService.kt
@@ -2,8 +2,13 @@ package com.playtogether_android.data.api.light
 
 import com.playtogether_android.data.model.response.light.ResponseLightListCategory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface LightService {
-    @GET("light/?category=VALUE&sort=VALUE")
-    suspend fun getLightListCategory(): ResponseLightListCategory
+
+    @GET("light/?category={category}&sort={sort}")
+    suspend fun getLightListCategory(
+        @Path("category") category: String,
+        @Path("sort") sort: String
+    ): ResponseLightListCategory
 }
