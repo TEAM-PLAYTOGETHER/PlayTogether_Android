22e35f8bf0be0727d019c7ed52d461f124efad15
garden <0401ah@gmail.com>
2022-05-10 22:59:23 +0900

    [CHORE] #15 - Adapter 연결 Test

--- app/src/main/java/com/playtogether_android/app/presentation/ui/thunder/TabLikeFragment.kt
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/thunder/TabLikeFragment.kt
@@ -5,56 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.playtogether_android.app.R
+import com.playtogether_android.app.databinding.FragmentTabApplyBinding
+import com.playtogether_android.app.databinding.FragmentTabLikeBinding
+import com.playtogether_android.app.presentation.base.BaseFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class TabLikeFragment : BaseFragment<FragmentTabLikeBinding>(R.layout.fragment_tab_like) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TabLikeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class TabLikeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var thunderListAdapter: ThunderListAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        thunderListAdapter = ThunderListAdapter()
+
+        thunderListAdapter.thunderList = listOf(
+            TempThunderListData.ThunderList("37", "신촌 곱창 선착 5명 구함", "2022.04.13", "14:00", 6, "신촌","1"),
+            TempThunderListData.ThunderList("37", "신촌 곱창 선착 5명 구함", "2022.04.13", "14:00", 6, "신촌","1"),
+            TempThunderListData.ThunderList("37", "신촌 곱창 선착 5명 구함", "2022.04.13", "14:00", 6, "신촌","1"),
+            TempThunderListData.ThunderList("37", "신촌 곱창 선착 5명 구함", "2022.04.13", "14:00", 6, "신촌","1"),
+            TempThunderListData.ThunderList("37", "신촌 곱창 선착 5명 구함", "2022.04.13", "14:00", 6, "신촌","1"),
+            TempThunderListData.ThunderList("37", "신촌 곱창 선착 5명 구함", "2022.04.13", "14:00", 6, "신촌","1"),
+            TempThunderListData.ThunderList("37", "신촌 곱창 선착 5명 구함", "2022.04.13", "14:00", 6, "신촌","1"),
+            TempThunderListData.ThunderList("37", "신촌 곱창 선착 5명 구함", "2022.04.13", "14:00", 6, "신촌","1"),
+
+            )
+
+        with(binding.rvLikeThunderList){
+            layoutManager = LinearLayoutManager(requireActivity())
+            adapter = thunderListAdapter
         }
+
+        thunderListAdapter.notifyDataSetChanged()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_like, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TabLikeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TabLikeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
