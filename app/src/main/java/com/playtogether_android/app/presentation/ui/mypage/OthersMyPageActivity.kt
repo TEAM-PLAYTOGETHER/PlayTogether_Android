ce18f51ac115f34373fe84c2eb68eddb138a5655
lhb8106 <lhb8106@naver.com>
2022-05-13 20:45:55 +0900

    [FEAT] #98 mypage view end

---
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/mypage/OthersMyPageActivity.kt
@@ -0,0 +1,14 @@
+package com.playtogether_android.app.presentation.ui.mypage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.playtogether_android.app.R
+import com.playtogether_android.app.databinding.ActivityFirstOnBoardingBinding
+import com.playtogether_android.app.presentation.base.BaseActivity
+
+class OthersMyPageActivity : BaseActivity<ActivityFirstOnBoardingBinding>(R.layout.activity_others_my_page) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
