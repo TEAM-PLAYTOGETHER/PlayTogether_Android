7f0e766a4de14df992749c5d0d3d3b8a62dc19c6
lhb8106 <lhb8106@naver.com>
2022-05-13 20:09:56 +0900

    [FEAT] #96 - add splash xml

---
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/main/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.playtogether_android.app.presentation.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.playtogether_android.app.R
+import com.playtogether_android.app.databinding.ActivitySplashBinding
+import com.playtogether_android.app.presentation.base.BaseActivity
+
+class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) { {
+}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
