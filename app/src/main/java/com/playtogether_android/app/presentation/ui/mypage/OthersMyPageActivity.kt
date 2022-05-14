a6d2d99212403ef2a76564b8af31f0b42d0346ae
lhb8106 <lhb8106@naver.com>
2022-05-15 00:00:42 +0900

    [FEAT] #120 - add chatting code

--- app/src/main/java/com/playtogether_android/app/presentation/ui/mypage/OthersMyPageActivity.kt
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/mypage/OthersMyPageActivity.kt
@@ -1,23 +1,28 @@
 package com.playtogether_android.app.presentation.ui.mypage
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import com.playtogether_android.app.R
 import com.playtogether_android.app.databinding.ActivityOthersMyPageBinding
 import com.playtogether_android.app.presentation.base.BaseActivity
+import com.playtogether_android.app.presentation.ui.message.ChattingActivity
 import com.playtogether_android.app.presentation.ui.mypage.viewModel.MyPageViewModel
+import com.playtogether_android.app.presentation.ui.thunder.viewmodel.ThunderDetailViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class OthersMyPageActivity :
     BaseActivity<ActivityOthersMyPageBinding>(R.layout.activity_others_my_page) {
 
     private val myPageViewModel: MyPageViewModel by viewModel()
+    private val thunderDetailViewModel: ThunderDetailViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initPersonalInfo()
         initBackBtn()
+        setClickListener()
     }
 
     //내 정보 서버통신
@@ -41,5 +46,24 @@ class OthersMyPageActivity :
         binding.ivOtherMypageBack.setOnClickListener {
             finish()
         }
+    }
+
+    private fun setClickListener() {
+
+        binding.ivOtherMypageMessage.setOnClickListener {
+//           쪽지 보내기로 이동
+            var organizerId = -1
+            var name = "null"
+            thunderDetailViewModel.organizerInfo.observe(this) {
+                organizerId = it.organizerId
+                name = it.name
+            }
+            val intent = Intent(this, ChattingActivity::class.java)
+            intent.putExtra("audienceId", organizerId)
+            intent.putExtra("name", name)
+            startActivity(intent)
+        }
+
+
     }
 }
