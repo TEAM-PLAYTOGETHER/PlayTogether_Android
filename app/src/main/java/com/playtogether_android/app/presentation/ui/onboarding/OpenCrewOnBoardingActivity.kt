861482a4dd0f301bcbf9a3080b0815af527fc276
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-06-30 13:38:15 +0900

    [FEAT] add string

---
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/onboarding/OpenCrewOnBoardingActivity.kt
@@ -0,0 +1,12 @@
+package com.playtogether_android.app.presentation.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.playtogether_android.app.R
+
+class OpenCrewOnBoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_crew_on_boarding)
+    }
+}
