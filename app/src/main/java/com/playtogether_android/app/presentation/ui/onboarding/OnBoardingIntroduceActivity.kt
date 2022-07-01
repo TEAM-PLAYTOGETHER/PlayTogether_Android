1d992ecc97145adf73928aa5a6505397eb8a02a3
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-07-01 16:47:53 +0900

    [FEAT] #146 - view end

---
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/onboarding/OnBoardingIntroduceActivity.kt
@@ -0,0 +1,12 @@
+package com.playtogether_android.app.presentation.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.playtogether_android.app.R
+
+class OnBoardingIntroduceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding_introduce)
+    }
+}
