4d85b3bc6484dccec38dffe36016e508819c6215
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-07-02 14:25:32 +0900

    [FEAT] #148 - add subway code

--- app/src/main/java/com/playtogether_android/app/presentation/ui/onboarding/SearchSubwayActivity.kt
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/onboarding/SearchSubwayActivity.kt
@@ -3,10 +3,27 @@ package com.playtogether_android.app.presentation.ui.onboarding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.playtogether_android.app.R
+import com.playtogether_android.app.databinding.ActivitySearchSubwayBinding
+import com.playtogether_android.app.presentation.base.BaseActivity
 
-class SearchSubwayActivity : AppCompatActivity() {
+class SearchSubwayActivity : BaseActivity<ActivitySearchSubwayBinding>(R.layout.activity_search_subway) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_subway)
+
+    }
+
+    private fun initNetwork() {
+        // 키 값
+        val key = "키값"
+        // 현재 페이지번호
+        val pageNo = "&pageNo=1"
+        // 한 페이지 결과 수
+        val numOfRows ="&numOfRows=5"
+        // AND(안드로이드)
+        val MobileOS = "&MobileOS=AND"
+        // 서비스명 = 어플명
+        val MobileApp = "&MobileApp=AppTest"
+        // API 정보를 가지고 있는 주소
+        val url = "http://api.visitkorea.or.kr/openapi/service/rest/GoCamping/basedList?serviceKey="+key+pageNo+numOfRows+MobileOS+MobileApp
     }
 }
