0fe540438e8d3e6c55218ff0b06d9ac953d3e3bc
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-07-26 04:59:55 +0900

    [FEAT] #156 - create view

---
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/onboarding/OnboardingReDownLoadActivity.kt
@@ -0,0 +1,13 @@
+package com.playtogether_android.app.presentation.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.playtogether_android.app.R
+import com.playtogether_android.app.databinding.ActivityOnboardingReDownLoadBinding
+import com.playtogether_android.app.presentation.base.BaseActivity
+
+class OnboardingReDownLoadActivity : BaseActivity<ActivityOnboardingReDownLoadBinding>(R.layout.activity_onboarding_re_down_load) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
