9d804cba3744c64ea21244087927af61b3ddb9d4
권용민 <briandr@naver.com>
2022-08-04 04:30:40 +0900

    [FEAT] #142 chatting - combine viewmodel, add 2 way data binding

--- app/src/main/java/com/playtogether_android/app/presentation/ui/message/viewmodel/ChatViewModel.kt
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/message/viewmodel/ChatViewModel.kt
@@ -6,24 +6,33 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.playtogether_android.domain.model.message.ChatData
+import com.playtogether_android.domain.model.message.GetSendMessageData
+import com.playtogether_android.domain.model.message.PostSendMessageData
 import com.playtogether_android.domain.usecase.message.GetChatUseCase
+import com.playtogether_android.domain.usecase.message.PostSendMessageUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ChatViewModel @Inject constructor(
-    val getChatUseCase: GetChatUseCase
+    val getChatUseCase: GetChatUseCase,
+    val postSendMessageUseCase: PostSendMessageUseCase
 ) : ViewModel() {
     private var _chatData = MutableLiveData<List<ChatData>>()
     val chatData: LiveData<List<ChatData>> get() = _chatData
+
+    private val _getSendMessage = MutableLiveData<GetSendMessageData>()
+    val getSendMessage: LiveData<GetSendMessageData> get() = _getSendMessage
+
+    val inputMessage = MutableLiveData<String>()
 
     fun getChatList(roomId: Int) {
         viewModelScope.launch {
             kotlin.runCatching { getChatUseCase(roomId) }
                 .onSuccess {
                     //_chatData.value=it
-                    var tempList: List<ChatData> = it
+                    val tempList: List<ChatData> = it
                     for (i in it.indices) {
                         var date = it[i].time.slice(IntRange(0, 9))
                         date = date.replace("-", ".")
@@ -35,6 +44,21 @@ class ChatViewModel @Inject constructor(
                 }
                 .onFailure { error ->
                     Log.d("messageServer", "채팅 읽어오기 실패")
+                }
+        }
+    }
+
+    fun postSendMessage(postSendMessageData: PostSendMessageData) {
+        viewModelScope.launch {
+            kotlin.runCatching { postSendMessageUseCase(postSendMessageData) }
+                .onSuccess {
+                    _getSendMessage.value = it
+                    Log.d("messageServer", "메시지 보내기 서버통신 성공")
+                }
+                .onFailure {
+                    _getSendMessage.value = GetSendMessageData("false", false)
+                    it.printStackTrace()
+                    Log.d("messageServer", "메시지 보내기 서버통신 실패")
                 }
         }
     }
