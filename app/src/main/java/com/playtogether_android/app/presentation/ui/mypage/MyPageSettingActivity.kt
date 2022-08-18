2f5319d39ff441c7ccbc4020018af23c8b9c7c90
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-08-19 01:27:49 +0900

    [FEAT] #184 - add web view Test Code

--- app/src/main/java/com/playtogether_android/app/presentation/ui/mypage/MyPageSettingActivity.kt
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/mypage/MyPageSettingActivity.kt
@@ -1,12 +1,26 @@
 package com.playtogether_android.app.presentation.ui.mypage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.playtogether_android.app.R
+import com.playtogether_android.app.databinding.ActivityMyPageSettingBinding
+import com.playtogether_android.app.databinding.ActivityWebViewBinding
+import com.playtogether_android.app.presentation.base.BaseActivity
+import com.playtogether_android.app.presentation.ui.main.WebViewActivity
 
-class MyPageSettingActivity : AppCompatActivity() {
+class MyPageSettingActivity : BaseActivity<ActivityMyPageSettingBinding>(R.layout.activity_my_page_setting) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_page_setting)
+        initIntent()
+
+    }
+
+    private fun initIntent() {
+        binding.tvSettingGuide.setOnClickListener {
+            val intent = Intent(this, WebViewActivity::class.java)
+            intent.putExtra("url", "www.naver.com")
+            startActivity(intent)
+        }
     }
 }
