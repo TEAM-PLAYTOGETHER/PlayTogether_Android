1472497f0f4295185bd6db17c19394f5230a6c8d
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-08-19 01:09:07 +0900

    [FEAT] #184 - view end

---
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/mypage/MyPageSettingActivity.kt
@@ -0,0 +1,12 @@
+package com.playtogether_android.app.presentation.ui.mypage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.playtogether_android.app.R
+
+class MyPageSettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page_setting)
+    }
+}
