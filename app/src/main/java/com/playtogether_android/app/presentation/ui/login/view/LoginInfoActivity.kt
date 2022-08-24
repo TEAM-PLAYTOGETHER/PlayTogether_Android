646bd8e2f2b30cecd4ad46a9558d536609b03cf6
username <ieejo716@naver.com>
2022-08-24 17:15:56 +0900

    [FEAT]: 정보 입력 뷰 완료 , 회원 가입 api나오면 다시 작업할 예정

--- app/src/main/java/com/playtogether_android/app/presentation/ui/login/view/LoginInfoActivity.kt
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/login/view/LoginInfoActivity.kt
@@ -1,21 +1,14 @@
 package com.playtogether_android.app.presentation.ui.login.view
 
-import android.app.AlertDialog
-import android.app.DatePickerDialog
 import android.app.Dialog
-import android.app.TimePickerDialog
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.WindowManager
-import android.widget.NumberPicker
-import androidx.core.content.ContextCompat
-import androidx.databinding.DataBindingUtil
+import android.widget.Button
 import com.playtogether_android.app.R
 import com.playtogether_android.app.databinding.ActivityLoginInfoBinding
 import com.playtogether_android.app.databinding.DialogOnlyYearBinding
 import com.playtogether_android.app.presentation.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.*
 
 @AndroidEntryPoint
 class LoginInfoActivity : BaseActivity<ActivityLoginInfoBinding>(R.layout.activity_login_info) {
@@ -26,13 +19,29 @@ class LoginInfoActivity : BaseActivity<ActivityLoginInfoBinding>(R.layout.activi
     }
 
     private fun initView() {
-        binding.btnLogininfoMan.apply {
-            setOnClickListener {
-                this.isSelected = !this.isSelected
-            }
-        }
+        val list = listOf(binding.btnLogininfoMan, binding.btnLogininfoWoman)
+        setGenderClickListener(list)
         binding.clLogininfoDateContainer.setOnClickListener {
             initOnlyYearDatePickerDialog()
+        }
+        backButtonListener()
+    }
+
+    private fun setGenderClickListener(list: List<Button>) {
+        list[0].setOnClickListener {
+            it.isSelected = true
+            list[1].isSelected = false
+        }
+        list[1].setOnClickListener {
+            it.isSelected = true
+            list[0].isSelected = false
+        }
+
+    }
+
+    private fun backButtonListener() {
+        binding.ivLogintermBack.setOnClickListener {
+            finish()
         }
     }
 
@@ -43,8 +52,19 @@ class LoginInfoActivity : BaseActivity<ActivityLoginInfoBinding>(R.layout.activi
         val params = dialog.window?.attributes
         params?.width = WindowManager.LayoutParams.MATCH_PARENT
         params?.height = WindowManager.LayoutParams.WRAP_CONTENT
-        dialogBinding.tvDialogAccess.setOnClickListener {
 
+        dialogBinding.tvDialogAccess.setOnClickListener {
+            with(binding) {
+                tvLogininfoDateText.apply {
+                    this.text = dialogBinding.npDialogPicker.value.toString()
+                    this.setTextAppearance(R.style.logininfo_year_text)
+                }
+                clLogininfoDateContainer.setBackgroundResource(R.drawable.rectangle_border_gray01_radius_10)
+                tvLogininfoDateText.text = dialogBinding.npDialogPicker.value.toString()
+                ivLoginDate.setImageResource(R.drawable.ic_icn_calendar)
+            }
+            btnAccessButton()
+            dialog.dismiss()
         }
         dialogBinding.tvDialogCancel.setOnClickListener {
             dialog.dismiss()
@@ -55,10 +75,14 @@ class LoginInfoActivity : BaseActivity<ActivityLoginInfoBinding>(R.layout.activi
             minValue = 1900
             maxValue = 2022
             value = 2000
-
         }
-
-
         dialog.show()
+    }
+
+    private fun btnAccessButton() {
+        binding.btnLogininfoAccess.apply {
+            this.isClickable = true
+            this.isSelected = true
+        }
     }
 }
