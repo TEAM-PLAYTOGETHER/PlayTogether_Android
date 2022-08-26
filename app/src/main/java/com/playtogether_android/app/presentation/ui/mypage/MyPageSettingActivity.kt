900f522794f0e16ea86e36fe16a73f5a03a5ea34
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-08-26 13:58:40 +0900

    [FEAT] #199 - add mypage setting intent

--- app/src/main/java/com/playtogether_android/app/presentation/ui/mypage/MyPageSettingActivity.kt
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/mypage/MyPageSettingActivity.kt
@@ -12,15 +12,40 @@ import com.playtogether_android.app.presentation.ui.main.WebViewActivity
 class MyPageSettingActivity : BaseActivity<ActivityMyPageSettingBinding>(R.layout.activity_my_page_setting) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initIntent()
-
+        initMovePage()
     }
 
-    private fun initIntent() {
+    private fun initIntent(url: String) {
+        val intent = Intent(this, WebViewActivity::class.java)
+        intent.putExtra("url", url)
+        startActivity(intent)
+    }
+
+
+    private fun initMovePage() {
+        //버전정보
+        binding.tvSettingVersion.setOnClickListener {
+            initIntent("https://cheddar-liquid-051.notion.site/14fc6c632471488486e7e76bc161069e")
+        }
+
+        //이용가이드
         binding.tvSettingGuide.setOnClickListener {
-            val intent = Intent(this, WebViewActivity::class.java)
-            intent.putExtra("url", "www.naver.com")
-            startActivity(intent)
+            initIntent("https://cheddar-liquid-051.notion.site/1a22db662cb5416caaf6d08b58e98b1a")
+        }
+
+        //정책 및 약관
+        binding.tvSettingPolicy.setOnClickListener {
+            initIntent("https://cheddar-liquid-051.notion.site/fc9dae6b1917453bbb1a39c4be4e4297")
+        }
+
+        //개발자 정보
+        binding.tvSettingDeveloper.setOnClickListener {
+            initIntent("https://cheddar-liquid-051.notion.site/9b109514a74349f4988c9b7f72fe4e47")
+        }
+
+        //신고하기
+        binding.tvSettingWarn.setOnClickListener {
+            initIntent("https://forms.gle/7deZ5JgtVqrbTifG8")
         }
     }
 }
