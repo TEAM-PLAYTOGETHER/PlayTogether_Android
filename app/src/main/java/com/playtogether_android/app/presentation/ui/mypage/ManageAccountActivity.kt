e7959e42169cff6f14d8e8c31b2f015a93134255
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-08-29 00:34:36 +0900

    [FEAT] #203 - manage account view end

---
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/mypage/ManageAccountActivity.kt
@@ -0,0 +1,13 @@
+package com.playtogether_android.app.presentation.ui.mypage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.playtogether_android.app.R
+import com.playtogether_android.app.databinding.ActivityManageAccountBinding
+import com.playtogether_android.app.presentation.base.BaseActivity
+
+class ManageAccountActivity : BaseActivity<ActivityManageAccountBinding>(R.layout.activity_manage_account) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
