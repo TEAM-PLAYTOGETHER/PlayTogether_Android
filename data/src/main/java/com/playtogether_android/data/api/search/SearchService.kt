cf5a9deae4ba32ec13bd1e3189f6f30310cc134e
k_dragonm <briandr@naver.com>
2022-08-31 00:18:20 +0900

    [FEAT] #206 - add searching service parameter

--- data/src/main/java/com/playtogether_android/data/api/search/SearchService.kt
+++ data/src/main/java/com/playtogether_android/data/api/search/SearchService.kt
@@ -2,11 +2,15 @@ package com.playtogether_android.data.api.search
 
 import com.playtogether_android.data.model.response.search.SearchResult
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface SearchService {
-    @GET("light/search")
+    @GET("light/{crewId}/{curpage}/{pageSize}/search")
     suspend fun getSearchResult(
+        @Path("crewId") crewId : String,
+        @Path("curpage") curpage : Int?,
+        @Path("pageSize") pageSize : Int?,
         @Query("search") search: String,
         @Query("category") category: String?
     ): SearchResult
