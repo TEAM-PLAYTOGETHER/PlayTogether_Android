4f563d944b60e8ba45842e1354aa7dae3531979b
username <ieejo716@naver.com>
2022-08-31 15:38:44 +0900

    [CHORE] : 소셜 로그인 관련 service, datasource, repository, dataclass

---
+++ domain/src/main/java/com/playtogether_android/domain/model/sign/IssuanceItem.kt
@@ -0,0 +1,6 @@
+package com.playtogether_android.domain.model.sign
+
+data class IssuanceItem(
+    val accessToken : String,
+    val refreshToken : String
+)
---
+++ domain/src/main/java/com/playtogether_android/domain/model/sign/UserInfo.kt
@@ -0,0 +1,6 @@
+package com.playtogether_android.domain.model.sign
+
+data class UserInfo(
+    val gender: String,
+    val birth: Int
+)
