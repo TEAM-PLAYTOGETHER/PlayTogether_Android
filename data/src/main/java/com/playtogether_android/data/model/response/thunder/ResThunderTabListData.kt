471139f86ed08d56a14e805ef27b67120c33e869
username <ieejo716@naver.com>
2022-09-04 20:53:51 +0900

    [REFACTOR] : 번개 탭 서비스, 데이터 관련 변경 사항 수정

--- data/src/main/java/com/playtogether_android/data/model/response/thunder/ResThunderTabListData.kt
+++ data/src/main/java/com/playtogether_android/data/model/response/thunder/ResThunderTabListData.kt
@@ -4,30 +4,34 @@ package com.playtogether_android.data.model.response.thunder
 import com.google.gson.annotations.SerializedName
 
 data class ResThunderTabListData(
-    @SerializedName("status")
-    val status: Int,
-    @SerializedName("success")
-    val success: Boolean,
-    @SerializedName("message")
     val message: String,
-    @SerializedName("data")
-    val data: List<Data>
+    val status: Int,
+    val success: Boolean,
+    val data: Data,
 ) {
     data class Data(
-        @SerializedName("light_id")
-        val lightId: Int,
-        @SerializedName("title")
-        val title: String,
-        val category: String,
-        @SerializedName("date")
-        val date: String,
-        @SerializedName("time")
-        val time: String,
-        @SerializedName("people_cnt")
-        val peopleCnt: Int,
-        @SerializedName("place")
-        val place: String,
-        @SerializedName("LightMemberCnt")
-        val lightMemberCnt: Int
-    )
+        val lightData: List<LightData>,
+        val limit: Int,
+        val offset: Int,
+        val totalCount: Int,
+        val totalPage: Int
+    ) {
+        data class LightData(
+            val category: String,
+            val date: String,
+            @SerializedName("is_opened")
+            val isOpened: Boolean,
+            @SerializedName("light_id")
+            val lightId: Int,
+            @SerializedName("LightMemberCnt")
+            val lightMemberCnt: Int,
+            @SerializedName("people_cnt")
+            val peopleCnt: Int,
+            val place: String,
+            @SerializedName("scp_cnt")
+            val scpCnt: Int,
+            val time: String,
+            val title: String
+        )
+    }
 }
--- domain/src/main/java/com/playtogether_android/domain/usecase/thunder/GetApplyListUseCase.kt
+++ domain/src/main/java/com/playtogether_android/domain/usecase/thunder/GetApplyListUseCase.kt
@@ -1,13 +1,14 @@
 package com.playtogether_android.domain.usecase.thunder
 
+import com.playtogether_android.domain.model.light.CategoryData
 import com.playtogether_android.domain.model.thunder.ThunderTabListData
 import com.playtogether_android.domain.repository.thunder.ThunderRepository
 import javax.inject.Inject
 
 class GetApplyListUseCase @Inject constructor(private val repository: ThunderRepository) {
 
-    suspend operator fun invoke(): ThunderTabListData {
-        return repository.getApplyList()
+    suspend operator fun invoke(): List<CategoryData> {
+        return repository.getApplyList().data
     }
 
 }
