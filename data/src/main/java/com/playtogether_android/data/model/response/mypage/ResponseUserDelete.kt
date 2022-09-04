1fd9b3fccf6f3f7cd29a4e40c2418ac02913b04e
k_dragonm <briandr@naver.com>
2022-09-05 00:07:54 +0900

    [FEAT] #206 - modify git pull error(mypage)

---
+++ data/src/main/java/com/playtogether_android/data/model/response/mypage/ResponseUserDelete.kt
@@ -0,0 +1,7 @@
+package com.playtogether_android.data.model.response.mypage
+
+data class ResponseUserDelete(
+    val message: String,
+    val status: Int,
+    val success: Boolean
+)
---
+++ domain/src/main/java/com/playtogether_android/domain/model/mypage/UserDeleteData.kt
@@ -0,0 +1,6 @@
+package com.playtogether_android.domain.model.mypage
+
+data class UserDeleteData(
+    val message: String,
+    val success: Boolean
+)
