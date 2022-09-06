bd770b3137fe6d17971debbb2cea84f64d4b7ba1
garden <0401ah@gmail.com>
2022-09-07 07:29:12 +0900

    [UI] 차단사용자 관리하기 뷰 완성 (#207)

---
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/userInfo/ManageBlockedUserActivity.kt
@@ -0,0 +1,14 @@
+package com.playtogether_android.app.presentation.ui.userInfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.playtogether_android.app.R
+import com.playtogether_android.app.databinding.ActivityManageBlockedUserBinding
+import com.playtogether_android.app.presentation.base.BaseActivity
+
+class ManageBlockedUserActivity : BaseActivity<ActivityManageBlockedUserBinding>(R.layout.activity_manage_blocked_user) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
