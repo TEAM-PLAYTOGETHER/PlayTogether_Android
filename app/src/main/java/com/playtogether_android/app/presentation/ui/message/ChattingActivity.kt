3ea4ec65ce37e1436b54c507edad37da7d2f5315
권용민 <briandr@naver.com>
2022-09-16 00:46:01 +0900

    [FEAT] #223 - modify chatting activity socket communication

--- app/src/main/java/com/playtogether_android/app/presentation/ui/message/ChattingActivity.kt
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/message/ChattingActivity.kt
@@ -3,22 +3,13 @@ package com.playtogether_android.app.presentation.ui.message
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
-import androidx.core.content.ContextCompat
-import com.google.gson.Gson
 import com.playtogether_android.app.R
 import com.playtogether_android.app.databinding.ActivityChattingBinding
 import com.playtogether_android.app.presentation.base.BaseActivity
-import com.playtogether_android.app.presentation.ui.message.data.SendMessageSocket
-import com.playtogether_android.app.presentation.ui.message.data.SubscribeSocket
 import com.playtogether_android.app.presentation.ui.message.viewmodel.ChatViewModel
-import com.playtogether_android.app.util.AuthInterceptor
-import com.playtogether_android.domain.model.message.ChatData
+import com.playtogether_android.app.util.shortToast
 import dagger.hilt.android.AndroidEntryPoint
-import io.socket.client.IO
-import io.socket.client.Socket
-import io.socket.emitter.Emitter
-import okhttp3.OkHttpClient
-import java.net.URISyntaxException
+import timber.log.Timber
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -26,11 +17,6 @@ import java.util.*
 class ChattingActivity : BaseActivity<ActivityChattingBinding>(R.layout.activity_chatting) {
     private val chatViewModel: ChatViewModel by viewModels()
     private lateinit var chatAdapter: ChatAdapter
-    private lateinit var socket: Socket
-    private val gson = Gson()
-    private var roomId = -1
-    private var audienceId = -1
-    private lateinit var name: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,10 +24,9 @@ class ChattingActivity : BaseActivity<ActivityChattingBinding>(R.layout.activity
         binding.viewmodel = chatViewModel
         binding.lifecycleOwner = this
 
-        getIntentData()
-        getChatAll()
+        setName()
         initAdapter()
-        changeSendImage()
+        updateChatUi()
 
         clickSendMessage()
         clickBackArrow()
@@ -49,78 +34,40 @@ class ChattingActivity : BaseActivity<ActivityChattingBinding>(R.layout.activity
 
     override fun onResume() {
         super.onResume()
-        initSocket(roomId, audienceId)
-        Log.d("asdf", "socket connected")
+        val roomId = intent.getIntExtra("roomId", -1)
+        val recvId = intent.getIntExtra("audienceId", -1)
+        if (roomId == -1) error("ChatActivity로 roomId를 넘겨주지 않음")
+        if (recvId == -1) error("ChatActivity로 recvId를 넘겨주지 않음")
+        chatViewModel.getChatList(roomId)
+        connectSocketServer(roomId, recvId)
     }
 
     override fun onPause() {
         super.onPause()
-        socket.disconnect()
-        Log.d("asdf", "socket disconnected")
+        chatViewModel.reqExitRoom()
+        chatViewModel.disconnect()
     }
 
-    fun initSocket(roomId: Int, audienceId: Int) {
-        try {
-            onConnect()
-        } catch (e: URISyntaxException) {
-            e.printStackTrace()
-            Log.d("asdf", "Connection Fail")
-        }
-        socket.connect()
-        emitSubscribe(roomId, audienceId)
-        onUpdateChat(chatAdapter)
-    }
-
-    private fun onConnect() { //서버에 jwt token을 넘겨주며 연결한다.
-        val httpClient = OkHttpClient.Builder().addNetworkInterceptor(AuthInterceptor()).build()
-        val options = IO.Options()
-        options.webSocketFactory = httpClient
-        options.callFactory = httpClient
-        socket = IO.socket("http://13.125.232.150:5500", options)
-        Log.d("asdf", "Connection success : ${socket.id()}")
-    }
-
-    private fun onUpdateChat(chatAdapter: ChatAdapter) { //상대방이 보내는 채팅을 소켓으로 받는다.
-        val onUpdateChat = Emitter.Listener {
-            val chat: SocketChatData =
-                gson.fromJson(it[0].toString(), SocketChatData::class.java)
-            val getChat = ChatData(
-                null,
-                content = chat.content,
-                messageType = false,
-                time = changeNowDateFormat()
-            )
+    private fun connectSocketServer(roomId: Int, recvId: Int) {
+        chatViewModel.initSocket(this)
+        chatViewModel.resConnect { socketErrorControl("resConnection") }
+        chatViewModel.reqEnterRoom(roomId, recvId)
+        chatViewModel.resEnterRoom { socketErrorControl("resEnterRoom") }
+        chatViewModel.resNewMessageToRoom()
+        chatViewModel.resSendMessage { runOnUiThread { shortToast("보내기에 실패하였습니다") } }
+        chatViewModel.resExitRoom {
             runOnUiThread {
-                chatAdapter.addChat(getChat)
-                scrollToBottom()
-                removeTimeAll()
+                finish()
+                Timber.e("채팅방을 나갔음에도 소켓 연결이 끊어지지 않음")
             }
         }
-        socket.on("newMessage", onUpdateChat)
     }
 
-    private fun emitSubscribe(roomId: Int, audienceId: Int) { //subscribe 이벤트를 emit한다.
-        Log.d("asdf", "id : $roomId $audienceId}")
-        val data = SubscribeSocket(roomId, audienceId)
-        val jsonData = gson.toJson(data)
-        socket.emit("subscribe", jsonData)
-    }
-
-    fun sendMessageSocket(text: String) {
-        val message = SendMessageSocket(text)
-        val jsonData = gson.toJson(message)
-        Log.d("asdf", "$message")
-        socket.emit("sendMessage", jsonData)
-
-        val sendChat = ChatData(
-            null,
-            content = message.messageContent,
-            messageType = true,
-            time = changeNowDateFormat()
-        )
+    private fun socketErrorControl(text: String) {
         runOnUiThread {
-            Log.d("asdf", "runOnUiThread 진입")
-            chatAdapter.addChat(sendChat)
+            shortToast("서버 통신이 원활하지 않습니다.")
+            finish()
+            Timber.e("채팅방 소켓 서버에 연결하지 못했습니다. ($text 오류")
         }
     }
 
@@ -132,81 +79,49 @@ class ChattingActivity : BaseActivity<ActivityChattingBinding>(R.layout.activity
     }
 
     private fun clickSendMessage() {
+        val recvId = intent.getIntExtra("audienceId", -1)
         binding.ivSendMessage.setOnClickListener {
-            sendMessageSocket(binding.etMessage.text.toString())
+            chatViewModel.reqSendMessage(binding.etMessage.text.toString(), recvId)
             binding.etMessage.text.clear()
+        }
+    }
+
+    private fun clickBackArrow() {
+        binding.ivInChattingBack.setOnClickListener { finish() }
+    }
+
+    private fun setName() {
+        val name = intent.getStringExtra("name") ?: error("ChatActivity로 name을 넘겨주지 않음")
+        binding.tvInChattingName.text = name
+    }
+
+    private fun updateChatUi() {
+        chatViewModel.chatData.observe(this) {
+            chatAdapter.submitList(it)
             scrollToBottom()
             removeTimeAll()
         }
     }
 
-    private fun clickBackArrow() {
-        binding.ivInChattingBack.setOnClickListener {
-            finish()
-        }
-    }
-
-    private fun changeSendImage() {
-        chatViewModel.inputMessage.observe(this) {
-            if (it.isNullOrEmpty()) cantSendUiChange()
-            else canSendUiChange()
-        }
-    }
-
-    private fun cantSendUiChange() {
-        binding.ivSendMessage.setImageResource(R.drawable.ic_icn_message)
-        binding.ivSendMessage.setBackgroundColor(
-            ContextCompat.getColor(this@ChattingActivity, R.color.gray_gray03)
-        )
-        binding.ivSendMessage.isClickable = false
-    }
-
-    private fun canSendUiChange() {
-        binding.ivSendMessage.setImageResource(R.drawable.ic_icn_message_black)
-        binding.ivSendMessage.setBackgroundColor(
-            ContextCompat.getColor(this@ChattingActivity, R.color.main_green)
-        )
-        binding.ivSendMessage.isClickable = true
-    }
-
-    private fun getIntentData() {
-        roomId = intent.getIntExtra("roomId", -1)
-        name = intent.getStringExtra("name")!!
-        audienceId = intent.getIntExtra("audienceId", -1)
-        binding.tvInChattingName.text = name
-    }
-
-    private fun getChatAll() {
-        if (roomId != -1) {
-            chatViewModel.getChatList(roomId)
-            chatViewModel.chatData.observe(this) {
-                chatAdapter.chatList.addAll(it)
-                chatAdapter.notifyDataSetChanged()
-                scrollToBottom()
-                removeTimeAll()
-            }
-        }
-    }
-
     private fun scrollToBottom() {
-        val size = chatAdapter.chatList.size - 1
+        val size = chatAdapter.currentList.size - 1
         binding.rvInChattingChatting.scrollToPosition(size)
     }
 
     private fun removeTimeAll() {
-        var nowSize = chatAdapter.chatList.size - 1
+        var nowSize = chatAdapter.currentList.size - 1
         var tempSize = nowSize - 1
 
         if (tempSize < 0)
             return
 
         while (true) {
-            if (chatAdapter.chatList[tempSize].timeVisible == false) {
+            if (chatAdapter.currentList[tempSize].timeVisible == false) {
                 break
             }
-            if (chatAdapter.chatList[tempSize].messageType == chatAdapter.chatList[nowSize].messageType) {
-                if (chatAdapter.chatList[nowSize].time == chatAdapter.chatList[tempSize].time) {
-                    chatAdapter.chatList[tempSize].timeVisible = false
+            if (chatAdapter.currentList[tempSize].messageType == chatAdapter.currentList[nowSize].messageType) {
+                if (chatAdapter.currentList[nowSize].time == chatAdapter.currentList[tempSize].time) {
+                    chatAdapter.currentList[tempSize].timeVisible = false
                 }
             }
             nowSize = tempSize
