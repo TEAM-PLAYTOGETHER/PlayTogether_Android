ddf8781b53c2a34205f54eefa25b887ee2412b60
garden <0401ah@gmail.com>
2022-09-18 11:48:50 +0900

    [FEAT] 유저 멀티프로필 상세 조회 API 연결 (#207)

--- app/src/main/java/com/playtogether_android/app/presentation/ui/userInfo/OtherInfoActivity.kt
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/userInfo/OtherInfoActivity.kt
@@ -1,16 +1,81 @@
 package com.playtogether_android.app.presentation.ui.userInfo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModel
 import com.playtogether_android.app.R
 import com.playtogether_android.app.databinding.ActivityOtherInfoBinding
 import com.playtogether_android.app.presentation.base.BaseActivity
+import com.playtogether_android.app.presentation.ui.message.ChattingActivity
+import com.playtogether_android.app.presentation.ui.userInfo.viewmodel.UserInfoViewModel
+import com.playtogether_android.app.util.DateTimeUtil
+import com.playtogether_android.data.singleton.PlayTogetherRepository
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class OtherInfoActivity : BaseActivity<ActivityOtherInfoBinding>(R.layout.activity_other_info) {
+
+    private val userInfoViewModel: UserInfoViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val memberId = intent.getIntExtra("memberId", -1)
+        val crewId = PlayTogetherRepository.crewId
+        clickEvent()
+        getOtherInfo(crewId, memberId)
+        initData()
+        Timber.d("initOtherInfo : $crewId")
+    }
+
+    private fun clickEvent() {
+        moveChattingView()
+        btnBackEvent()
+        btnOption()
+    }
+
+    // 채팅뷰로 이동
+    private fun moveChattingView() {
+        binding.clOtherInfoBtnChatting.setOnClickListener {
+            startActivity(Intent(this, ChattingActivity::class.java))
+        }
+    }
+
+    // 뒤로가기 버튼
+    private fun btnBackEvent() {
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
+    }
+
+    // 옵션 버튼
+    //todo 옵션 차단/신고 기능
+    private fun btnOption() {
+        binding.btnOption.setOnClickListener {
+
+        }
+    }
+
+    private fun getOtherInfo(crewId: Int, memberId: Int) {
+        userInfoViewModel.getOtherInfo(crewId, memberId)
+    }
+
+    private fun initData() {
+        userInfoViewModel.otherInfoData.observe(this) {
+            val birth = DateTimeUtil.convertBirthFormat(it.birth)
+            val gender = it.gender
+            val genderFormat: String
+            if (gender == "남") genderFormat = "M"
+            else genderFormat = "W"
+
+            binding.birthAndGender = "${birth}년생 ・ $genderFormat"
+            binding.otherInfo = it
+            binding.tvOtherInfoTitle.text = "${it.nickname}님의 프로필"
+            binding.tvOtherInfoChatting.text = "${it.nickname}님과 채팅하기"
+        }
     }
 }
--- app/src/main/java/com/playtogether_android/app/presentation/ui/userInfo/viewmodel/UserInfoViewModel.kt
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/userInfo/viewmodel/UserInfoViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.playtogether_android.domain.model.userInfo.MyInfoData
+import com.playtogether_android.domain.model.userInfo.OtherInfoData
 import com.playtogether_android.domain.repository.userInfo.UserInfoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -18,6 +19,11 @@ class UserInfoViewModel @Inject constructor (private val userInfoRepository: Use
     private val _myInfoData = MutableLiveData<MyInfoData>()
     val myInfoData: LiveData<MyInfoData> = _myInfoData
 
+    private val _otherInfoData = MutableLiveData<OtherInfoData>()
+    val otherInfoData: LiveData<OtherInfoData> = _otherInfoData
+
+
+    // 유저 본인 멀티프로필 상세 조회
     fun getMyInfo() = viewModelScope.launch {
         kotlin.runCatching { userInfoRepository.getMyInfo() }
             .onSuccess {
@@ -27,6 +33,19 @@ class UserInfoViewModel @Inject constructor (private val userInfoRepository: Use
             .onFailure {
                 it.printStackTrace()
                 Timber.e("getMyInfo-server 실패 : $it")
+            }
+    }
+
+    // 유저 멀티프로필 상세 조회
+    fun getOtherInfo(crewId: Int, memberId: Int) = viewModelScope.launch {
+        kotlin.runCatching { userInfoRepository.getOtherInfo(crewId, memberId) }
+            .onSuccess {
+                _otherInfoData.postValue(it)
+                Timber.d("getOtherInfo-server 성공 : $it")
+            }
+            .onFailure {
+                it.printStackTrace()
+                Timber.e("getOtherInfo-server 실패 : $it")
             }
     }
 }
