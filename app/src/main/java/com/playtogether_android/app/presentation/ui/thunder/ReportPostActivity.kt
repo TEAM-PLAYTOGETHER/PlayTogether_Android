acc16b77a42af1a6312dc356df896da480b99a6b
garden <0401ah@gmail.com>
2022-10-03 19:44:02 +0900

    [CHORE] #246 - 번개 게시글 신고 뷰 수정

---
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/thunder/ReportPostActivity.kt
@@ -0,0 +1,14 @@
+package com.playtogether_android.app.presentation.ui.thunder
+
+import android.os.Bundle
+import com.playtogether_android.app.R
+import com.playtogether_android.app.databinding.ActivityReportPostBinding
+import com.playtogether_android.app.presentation.base.BaseActivity
+
+
+class ReportPostActivity : BaseActivity<ActivityReportPostBinding>(R.layout.activity_report_post) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
