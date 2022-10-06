db5b33d82d6af9af5fc8368a4b53e05b716db83c
권용민 <briandr@naver.com>
2022-10-07 01:46:15 +0900

    modify domain and data for pagination

--- data/src/main/java/com/playtogether_android/data/datasource/light/LightDataSource.kt
+++ data/src/main/java/com/playtogether_android/data/datasource/light/LightDataSource.kt
@@ -1,13 +1,15 @@
 package com.playtogether_android.data.datasource.light
 
-import com.playtogether_android.data.model.response.light.ResponseLightListCategory
 import com.playtogether_android.data.model.response.light.ResponseHomeLightning
+import com.playtogether_android.data.model.response.light.ResponseLightListCategory
 
 interface LightDataSource {
     suspend fun getLightListCategory(
         crewId: Int,
         category: String,
         sort: String,
+        currentPage: Int,
+        pageSize: Int
     ): ResponseLightListCategory
 
     suspend fun getNewLightning(crewId: Int): ResponseHomeLightning
