9bf3a4ae75190bb9505d35aa990660d3242895a3
garden <0401ah@gmail.com>
2022-10-23 21:30:35 +0900

    [FIX] #246 - 개설자 동아리 탈퇴 불가능 로직 추가

--- app/src/main/java/com/playtogether_android/app/presentation/ui/userInfo/DeleteMyCrewActivity.kt
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/userInfo/DeleteMyCrewActivity.kt
@@ -1,28 +1,23 @@
 package com.playtogether_android.app.presentation.ui.userInfo
 
 import android.content.Intent
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Spannable
-import android.text.SpannableString
 import android.text.SpannableStringBuilder
-import android.text.Spanned
 import android.text.style.ForegroundColorSpan
 import android.view.WindowManager
 import androidx.activity.viewModels
-import androidx.core.text.set
 import com.playtogether_android.app.R
 import com.playtogether_android.app.databinding.ActivityDeleteMyCrewBinding
 import com.playtogether_android.app.databinding.DialogCheckBinding
 import com.playtogether_android.app.databinding.DialogYesNoBinding
 import com.playtogether_android.app.presentation.base.BaseActivity
-import com.playtogether_android.app.presentation.ui.main.SplashActivity
+import com.playtogether_android.app.presentation.ui.home.viewmodel.HomeViewModel
 import com.playtogether_android.app.presentation.ui.onboarding.OnboardingReDownLoadActivity
 import com.playtogether_android.app.presentation.ui.userInfo.viewmodel.UserInfoViewModel
-import com.playtogether_android.app.util.CustomDialog
 import com.playtogether_android.app.util.CustomDialogSon
-import com.playtogether_android.data.singleton.PlayTogetherRepository.crewId
+import com.playtogether_android.app.util.shortToast
+import com.playtogether_android.data.singleton.PlayTogetherRepository
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
@@ -30,6 +25,7 @@ import timber.log.Timber
 class DeleteMyCrewActivity : BaseActivity<ActivityDeleteMyCrewBinding>(R.layout.activity_delete_my_crew) {
 
     private val userInfoViewModel: UserInfoViewModel by viewModels()
+    private val homeViewModel: HomeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,9 +34,8 @@ class DeleteMyCrewActivity : BaseActivity<ActivityDeleteMyCrewBinding>(R.layout.
         btnBackEvent()
         btnDeleteEvent()
 
-        val crewName = intent.getStringExtra("crewName")
-        binding.tvCrewName.text = crewName
-        Timber.d("crewName받는쪽 : $crewName")
+        binding.tvCrewName.text = PlayTogetherRepository.crewName
+        Timber.d("crewName받는쪽 : ${PlayTogetherRepository.crewName}")
     }
 
     private fun fontColorHighlight() {
@@ -88,8 +83,8 @@ class DeleteMyCrewActivity : BaseActivity<ActivityDeleteMyCrewBinding>(R.layout.
             }
             tvDialogYes.setOnClickListener {
                 //todo 동아리 탈퇴 API 연결
+                deleteExecute()
                 with(userInfoViewModel) {
-                    delCrew()
                     isDelete.observe(this@DeleteMyCrewActivity) {
                         if (it) {
                             showConfirmDialog(dialog)
@@ -118,6 +113,28 @@ class DeleteMyCrewActivity : BaseActivity<ActivityDeleteMyCrewBinding>(R.layout.
             finish()
             //todo 탈퇴완료하고 어디로 넘어갈까????
             startActivity(Intent(this, OnboardingReDownLoadActivity::class.java))
+        }
+    }
+
+    private fun deleteExecute() {
+        homeViewModel.getCrewList()
+        homeViewModel.crewList.observe(this) {
+            var crewIndex: Int = 0
+            var nowCrewId = PlayTogetherRepository.crewId
+            for (i in it.indices) {
+                if (it[i].id == nowCrewId) {
+                    crewIndex = i
+                }
+            }
+
+            Timber.d("crewIndex: ${crewIndex}")
+
+            if (it[crewIndex].isAdmin) {
+                shortToast("개설자는 동아리 탈퇴가 불가능 합니다.\n플투팀에 문의해주세요.")
+            } else with(userInfoViewModel) {
+                delCrew()
+            }
+
         }
     }
 
