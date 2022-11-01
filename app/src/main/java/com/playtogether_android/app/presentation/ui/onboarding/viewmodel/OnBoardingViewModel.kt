7f97551e566b75c21e3c49596d6c055d7e58fcff
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-11-01 22:08:51 +0900

    불필요한 코드 삭제

--- app/src/main/java/com/playtogether_android/app/presentation/ui/onboarding/viewmodel/OnBoardingViewModel.kt
+++ app/src/main/java/com/playtogether_android/app/presentation/ui/onboarding/viewmodel/OnBoardingViewModel.kt
@@ -219,30 +219,6 @@ class OnBoardingViewModel @Inject constructor(
     //동아리 가입 가능 여부
     fun getCheckExist(crewCode: String) {
         viewModelScope.launch {
-            /*
-            when (val checkExist =
-                safeApiCall(Dispatchers.IO) { getCheckExistUseCase(crewCode) }) {
-                is ResultWrapper.Success -> {
-                    _checkExist.value =
-                        CheckExistData(
-                            true,
-                            checkExist.data.available,
-                            checkExist.data.id,
-                            checkExist.data.message,
-                            checkExist.data.name
-                        )
-                    Timber.e("모지2 : ${checkExist.data.message}")
-                }
-                is ResultWrapper.GenericError -> {
-                    Timber.e("모지 : ${checkExist.message}")
-                    _checkExist.value =
-                        CheckExistData(true, false, 0, checkExist.message.toString(), "")
-                }
-            }
-
-             */
-
-
         kotlin.runCatching { getCheckExistUseCase(crewCode) }
             .onSuccess {
                 _checkExist.value = it
